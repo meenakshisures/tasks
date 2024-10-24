8c51e7b8588681d041e60550eb6d8f21abf2a9ed
smeenakshi <meenakshi.s@inapp.com>
2024-10-24 14:40:20 +0530

    Updated after removing console

--- src/app/name-length.pipe.ts
+++ src/app/name-length.pipe.ts
@@ -6,8 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class NameLengthPipe implements PipeTransform {
 
   transform(value: any, length: number,y:any): unknown {
-    console.log(value,length,y);
-    console.log(value.slice(0,length));
+    
     return value.slice(0,length);
      
   }
